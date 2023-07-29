5fcab8cf67a353a90834f29c644bb34af51fd22f
westtly25 <westtly25@gmail.com>
2023-07-29 19:12:37 +0300

    Update

---
+++ Assets/Code/Runtime/Enemies System/EnemyConfigsContainer.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+namespace Assets.Code.Runtime.Enemies_System
+{
+    [CreateAssetMenu(fileName = "Enemy Configs Container", menuName = "Game / Configs / Enemy Configs Container", order = 1)]
+    public class EnemyConfigsContainer : ScriptableObject
+    {
+        [SerializeField]
+        public EnemyConfig[] EnemyConfig;
+    }
+}
