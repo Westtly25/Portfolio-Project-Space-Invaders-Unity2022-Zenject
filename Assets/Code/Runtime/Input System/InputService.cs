6ea37a38b3055b24fea6e0a2f6302a40901e9763
westtly25 <westtly25@gmail.com>
2023-08-03 14:18:43 +0300

    Update

--- Assets/Code/Runtime/Input System/InputService.cs
+++ Assets/Code/Runtime/Input System/InputService.cs
@@ -1,16 +1,52 @@
 ﻿using System;
+using Zenject;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 namespace Assets.Code.Scripts.Runtime.Input_System
 {
-    public class InputService
+    public class InputService : IInitializable, IDisposable
     {
-        public Vector3 Direction;
+        public Vector3 Direction { get; private set; }
 
-        public InputService()
+        private readonly GameInput inputActions;
+
+        public InputService() =>
+            this.inputActions = new GameInput();
+
+        public void Initialize() =>
+            InputSystem.onDeviceChange += InputSystem_onDeviceChange;
+
+        public void Dispose() =>
+            InputSystem.onDeviceChange -= InputSystem_onDeviceChange;
+
+        private void InputSystem_onDeviceChange(InputDevice arg1, InputDeviceChange arg2)
         {
-            
+            switch (arg2)
+            {
+                case InputDeviceChange.Added:
+                    break;
+                case InputDeviceChange.Removed:
+                    break;
+                case InputDeviceChange.Disconnected:
+                    break;
+                case InputDeviceChange.Reconnected:
+                    break;
+                case InputDeviceChange.Enabled:
+                    break;
+                case InputDeviceChange.Disabled:
+                    break;
+                case InputDeviceChange.UsageChanged:
+                    break;
+                case InputDeviceChange.ConfigurationChanged:
+                    break;
+                case InputDeviceChange.SoftReset:
+                    break;
+                case InputDeviceChange.HardReset:
+                    break;
+                default:
+                    break;
+            }
         }
     }
 }
